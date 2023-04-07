f07a16b97321f0c5273baa095be942b63b3ae677
hauzhengyi <88762262+hauzhengyi@users.noreply.github.com>
2023-04-07 23:21:00 +0800

    1 file modified, 8 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import './App.css';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Experience from './components/experience/Experience';
+import Projects from './components/projects/Projects';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <About />
+      <Contact />
+      <Experience />
+      <Projects />
+    </div>
+  );
 }
 
 export default App;
