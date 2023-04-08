e7b974c8546ce3d309b8324826bfbcb2965b437b
hauzhengyi <88762262+hauzhengyi@users.noreply.github.com>
2023-04-08 23:06:48 +0800

    2 files modified, 7 added

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import './App.css';
 import About from './components/about/About';
 import Contact from './components/contact/Contact';
 import Experience from './components/experience/Experience';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
 import Projects from './components/projects/Projects';
+import Skills from './components/skills/Skills';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Nav />
       <About />
-      <Contact />
       <Experience />
+      <Skills />
       <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Skills() {
+  return <div className="App">Skills</div>;
+}
+
+export default Skills;
