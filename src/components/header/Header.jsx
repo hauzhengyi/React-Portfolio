d5c1531cf2f612c2a633487a2ded6864572b0a85
hauzhengyi <88762262+hauzhengyi@users.noreply.github.com>
2023-04-09 12:06:39 +0800

    1 binaries uploaded

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 function Header() {
-  return <div className="App">Header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Zheng Yi</h1>
+        <h5 className="text-light">Frontend Developer</h5>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
