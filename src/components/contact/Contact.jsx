acbd18a872ce8896b342d3abe56e538fb064b496
hauzhengyi <88762262+hauzhengyi@users.noreply.github.com>
2023-04-19 00:13:57 +0800

    3 files modified

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,8 +4,56 @@ import React from 'react';
 function Contact() {
   return (
     <section id="contact">
-      <h5>Get In Touch</h5>
+      <h5>Don't Hesitate To</h5>
       <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <h4>Email</h4>
+            <a
+              href="mailto:hauzhengyi@hotmail.com"
+              className="contact__email"
+              target="blank"
+            >
+              hauzhengyi@hotmail.com
+            </a>
+          </article>
+          <article className="contact__option">
+            <h4>WhatsApp</h4>
+            <a
+              href="https://api.whatsapp.com/send?phone=60166731588"
+              className="contact__whatsapp"
+              target="blank"
+            >
+              WhatsApp
+            </a>
+          </article>
+          <article className="contact__option">
+            <h4>Phone</h4>
+            <a
+              href="tel:+60166731588"
+              className="contact__phone"
+              target="blank"
+            >
+              +60166731588
+            </a>
+          </article>
+        </div>
+        <form action="">
+          <input type="name" name="name" placeholder="Your Name" required />
+          <input type="email" name="name" placeholder="Your Email" required />
+          <textarea
+            type="email"
+            name="name"
+            placeholder="Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-cta">
+            Send Message
+          </button>
+        </form>
+      </div>
     </section>
   );
 }
