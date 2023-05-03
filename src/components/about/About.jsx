dfb75d759d82735c65cc8a74a8b98d5885e53eab
hauzhengyi <88762262+hauzhengyi@users.noreply.github.com>
2023-05-03 22:20:47 +0800

    1 file modified

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,11 +3,11 @@ import React, { useContext } from 'react';
 import { FaAward, FaFistRaised } from 'react-icons/fa';
 import { RiComputerFill } from 'react-icons/ri';
 import { Theme } from '/src/App';
-import photoPurple from '../../assets/About_Photo_Purple.jpeg';
-import photoYellow from '../../assets/About_Photo_Yellow.jpeg';
+import photoPurple from '../../assets/About_Photo_Purple.jpg';
+import photoYellow from '../../assets/About_Photo_Yellow.jpg';
 
 function About() {
-  const [theme, setTheme] = useContext(Theme); 
+  const [theme, setTheme] = useContext(Theme);
 
   return (
     <section id="about">
